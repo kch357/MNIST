214468ceb65e634fd8608f81b2c2295255bc1e91
Kang, ChungHyeok <kch30507@gmail.com>
2026-07-15 11:30:16 +0000

    t

--- my_study/te.py
+++ my_study/te.py
@@ -1,33 +1,52 @@
-class Man:
-    count = 0
+# coding: utf-8
+import sys, os
+sys.path.append(os.path.abspath("../deep-learning-from-scratch"))
+sys.path.append(os.path.abspath("../deep-learning-from-scratch/ch04"))
 
-    def __init__(self, name):
-        self.name = name
-        Man.count += 1
-        print("Initialized!")
-        print(f"Person count: {Man.count}")
+import numpy as np
+import matplotlib.pyplot as plt
+from dataset.mnist import load_mnist
+from two_layer_net import TwoLayerNet
 
-    def hello(self):
-        print("Hello " + self.name + "!")
-        print(f"Person count: {Man.count}")
+# 데이터 읽기
+(x_train, t_train), (x_test, t_test) = load_mnist(normalize=True, one_hot_label=True)
 
-    def goodbye(self):
-        print("Good-bye " + self.name + "!")
-        Man.count -= 1
-        print(f"Person count: {Man.count}")
+train_loss_list = []
 
+# 하이퍼파라미터
+iters_num = 10000  # 반복 횟수를 적절히 설정한다.
+train_size = x_train.shape[0]
+batch_size = 100   # 미니배치 크기
+learning_rate = 0.1
 
-m = Man("David")
-m = Man("Alice")
-Man.hello(m)  # m.hello() syntactic sugar
-Man.goodbye(m)
+network = TwoLayerNet(input_size=784, hidden_size=50, output_size=10)
 
-# a[3]
-# 3[a]
-# Obfuscation 난독화
+for i in range(iters_num):
+    # 미니배치 획득
+    batch_mask = np.random.choice(train_size, batch_size)
+    x_batch = x_train[batch_mask]
+    t_batch = t_train[batch_mask]
+    
+    # 기울기 계산
+    #grad = network.numerical_gradient(x_batch, t_batch)
+    grad = network.gradient(x_batch, t_batch)
+    
+    # 매개변수 갱신
+    for key in ('W1', 'b1', 'W2', 'b2'):
+        network.params[key] -= learning_rate * grad[key]
+    
+    # 학습 경과 기록
+    loss = network.loss(x_batch, t_batch)
+    train_loss_list.append(loss)
 
+# 그래프 그리기
+x = np.arange(len(train_loss_list))
+plt.plot(x, train_loss_list)
+plt.xlabel("iteration")
+plt.ylabel("loss")
+plt.ylim(0, 9)
+plt.xlim(0, 10000)
+plt.show()
 
-
-# *(a + 3)
-# 3[a]
-# *(3 + a)
+# 그래프 파일 저장
+plt.savefig('my_graph.png')
--- my_study/tes.py
+++ my_study/tes.py
@@ -1,9 +1,23 @@
-# 코드 상단에 아래를 추가해서 테스트해보세요
 import sys, os
 sys.path.append(os.path.abspath("../deep-learning-from-scratch"))
+sys.path.append(os.path.abspath("../deep-learning-from-scratch/ch04"))
 
-try:
-    from common.functions import sigmoid
-    print("성공: common 모듈을 찾았습니다!")
-except ImportError as e:
-    print(f"실패: 모듈을 찾을 수 없습니다. 경로를 확인하세요. 에러 내용: {e}")
+import numpy as np
+import matplotlib.pyplot as plt
+from dataset.mnist import load_mnist
+
+# 데이터 로드
+(x_train, t_train), (x_test, t_test) = load_mnist(normalize=True, one_hot_label=True)
+
+# 확인할 이미지의 인덱스 선택
+index = 0 
+img = x_train[index]
+label = t_train[index]
+
+# 데이터 형태 변환 (1차원 784배열 -> 28x28 2차원 배열)
+img = img.reshape(28, 28)
+
+# 이미지 시각화
+plt.imshow(img, cmap='gray')
+plt.title(f"Label: {np.argmax(label)}")
+plt.savefig('my_graph.png')
